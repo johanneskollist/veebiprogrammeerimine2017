e35b83c41845a051dcaecb263f7ba0bc862c7b0d
johannes kollist <johanneskollist@users.noreply.github.com>
2017-09-11 15:27:54 +0300

    lisatud uued PHP osad

--- esimene.php
+++ esimene.php
@@ -1,3 +1,33 @@
+<?php
+//muutujad
+$myName = "Johannes";
+$myFamilyName = "Kollist";
+
+$hourNow = date("h");
+
+$schoolDayStart = date("d.m.Y") ." " ."8.15";
+//echo $schoolDayStart;
+$schoolBegin = strtotime($schoolDayStart);
+//echo $schoolBegin;
+$timeNow = strtotime("now");
+//echo ($timeNow - $schoolBegin);
+$minutesPassed = round($timeNow - $schoolBegin) / 60;
+echo $minutesPassed;
+//echo $hournow;
+//võrdlen kellaaega ja annan hinnangu, mis päeva osaga on tegemist (<  >  ==  >=   <=   !=  )
+$partOfDay = "";
+if ( $hourNow < 8 ){
+	$partOfDay = "varajane hommik";
+}
+//echo $partOfDay
+if ( $hourNow >= 8 and $hourNow < 16 ) {
+	$partOfDay = "koolipäev";
+}
+if ( $hourNow >= 16 ) {
+	$partOfDay = "vaba aeg";
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,12 +37,16 @@
 	</title>
 </head>
 <body>
-	<h1>Johannes Kollist</h1>
+	<h1>
+	<?php
+	echo $myName ." " .$myFamilyName;
+	?>
+	</h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
 	<?php
 	echo "kõige esimene PHP abil väljastatud sõnum.";
 	echo "<p> Täna on ";
-	echo date ("d.m.Y");
+	echo date ("d.m.Y") . ", käes on " .$partOfDay;
 	echo ". <p>" ;
 	echo "<p> Lehe avamise hetkel oli kell " .date ("h:i:s") .".</p>";
 	?>
