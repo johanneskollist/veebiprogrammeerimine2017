61ca7ec57cea478b1ea1ed53753a4a2be5d7bd20
johannes kollist <johanneskollist@users.noreply.github.com>
2017-09-11 14:42:40 +0300

    Lisatud esimene PHP osa

--- esimene.php
+++ esimene.php
@@ -9,5 +9,12 @@
 <body>
 	<h1>Johannes Kollist</h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<?php
+	echo "kõige esimene PHP abil väljastatud sõnum.";
+	echo "<p> Täna on ";
+	echo date ("d.m.Y");
+	echo ". <p>" ;
+	echo "<p> Lehe avamise hetkel oli kell " .date ("h:i:s") .".</p>";
+	?>
 </body>
 </html>
