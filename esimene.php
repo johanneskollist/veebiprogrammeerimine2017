8009bf90bd08d2913f21bdc1a48e48a56392eee5
johannes kollist <johanneskollist@users.noreply.github.com>
2017-09-11 13:49:12 +0300

    esimene PHP fail, kus on vaid HTML

---
+++ esimene.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Johannes Kollist veebiprogemise asjad
+	</title>
+</head>
+<body>
+	<h1>Johannes Kollist</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+</body>
+</html>
