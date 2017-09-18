5e929469d403d67b6966efb8bef5b3b1db6d86fe
johannes kollist <johanneskollist@users.noreply.github.com>
2017-09-18 14:34:55 +0300

    lisatud vorm ja tsükkel

--- esimene.php
+++ esimene.php
@@ -2,6 +2,12 @@
 //muutujad
 $myName = "Johannes";
 $myFamilyName = "Kollist";
+$monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "november", "oktoober", "detsember"];
+
+//var_dump($monthNamesEt);
+
+//echo $monthNamesEt  [3];
+$monthNow = $monthNamesEt[date("n")- 1];
 
 $hourNow = date("h");
 
@@ -12,20 +18,54 @@ $schoolBegin = strtotime($schoolDayStart);
 $timeNow = strtotime("now");
 //echo ($timeNow - $schoolBegin);
 $minutesPassed = round($timeNow - $schoolBegin) / 60;
-echo $minutesPassed;
+//echo $minutesPassed;
 //echo $hournow;
 //võrdlen kellaaega ja annan hinnangu, mis päeva osaga on tegemist (<  >  ==  >=   <=   !=  )
 $partOfDay = "";
-if ( $hourNow < 8 ){
+//if ( $hourNow < 8 ){
 	$partOfDay = "varajane hommik";
-}
+
 //echo $partOfDay
-if ( $hourNow >= 8 and $hourNow < 16 ) {
+//if ( $hourNow >= 8 and $hourNow < 16 ) {
 	$partOfDay = "koolipäev";
-}
-if ( $hourNow >= 16 ) {
+
+//if ( $hourNow >= 16 ) {
 	$partOfDay = "vaba aeg";
-}
+	
+
+	//vanusega seotud muutujad
+	$myAge = 0;
+	$ageNote = "";
+	$myBirthYear;
+	$yearsOfMyLife = "";
+	
+	//echo $_POST;
+	//var_dump($_POST);
+	//echo $_POST{"birthYear"};
+	//arvutame vanuse
+	if ( isset($_POST["birthYear"]) and $_POST["birthYear"] != 0){
+		$myBirthYear = $_POST["birthYear"];
+		$myAge = date("Y") - $_POST{"birthYear"};
+		//echo $myAge;
+		$ageNote ="<p>Te olete umbes " .$myAge ." aastat vana.</p>";
+		
+		$yearsOfMyLife = "<ol> \n";
+		$yearNow = date("Y");
+		for ($i = $myBirthYear; $i <= $yearNow; $i ++){
+			$yearsOfMyLife .= "<li>" .$i ."</li> \n";
+		}
+		$yearsOfMyLife .= "</ol> \n";
+		
+	}
+	
+	
+	//lihtne tsükkel
+	/*for ($i = 0; $i < 5; $i ++){
+			echo "ha";
+	}*/
+	
+	
+	
 ?>
 
 <!DOCTYPE html>
@@ -46,9 +86,28 @@ if ( $hourNow >= 16 ) {
 	<?php
 	echo "kõige esimene PHP abil väljastatud sõnum.";
 	echo "<p> Täna on ";
-	echo date ("d.m.Y") . ", käes on " .$partOfDay;
+	echo date ("d.") .$monthNow .date(" Y") . ", käes on " .$partOfDay;
 	echo ". <p>" ;
 	echo "<p> Lehe avamise hetkel oli kell " .date ("h:i:s") .".</p>";
+	?>
+	<h2> Räägime vanusest</h2>
+	<p>Sisesta oma sünniaasta, arvutame vanuse!</p>
+	<form method='POST'>
+		<label>Teie sünniaasta: </label>
+		<input name="birthYear" id="birthYear" type="number" min="1900" max="2017" value="<?php echo $myBirthYear; ?>">
+		
+		<input id="submitBirthYear" type="submit" value="kinnita">
+		
+	</form>
+	<?php
+		if ( $ageNote != "") {
+			echo $ageNote;
+		}
+
+		if ( @yearsOfMyLife !=""){
+				echo "\n <h3> Olete elanud järgmistel aastatel</h3> \n" . $yearsOfMyLife;
+		}
+		
 	?>
 </body>
 </html>
